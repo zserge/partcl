ef41b9138fdf3520f034698f631073bcd340b79a
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2016-08-12 11:32:49 +0800

    safer repl, thanks to Alex Suykov!

--- tcl.c
+++ tcl.c
@@ -599,18 +599,29 @@ void tcl_destroy(struct tcl *tcl) {
 }
 
 #ifndef TEST
+#define CHUNK 1024
+
 int main() {
   struct tcl tcl;
-  char buf[1024] = {0};
+  int buflen = CHUNK;
+  char *buf = malloc(buflen);
   int i = 0;
 
   tcl_init(&tcl);
   while (1) {
-  next:
-    buf[i++] = fgetc(stdin);
+    int inp = fgetc(stdin);
+
+    if (i > buflen - 1)
+      buf = realloc(buf, buflen += CHUNK);
+
+    if (inp == 0 || inp == EOF)
+      break;
+
+    buf[i++] = inp;
+
     tcl_each(buf, i, 1) {
       if (p.token == TERROR && (p.to - buf) != i) {
-        memset(buf, 0, sizeof(buf));
+        memset(buf, 0, buflen);
         i = 0;
       } else if (p.token == TCMD && *(p.from) != '\0') {
         int r = tcl_eval(&tcl, buf, strlen(buf));
@@ -621,12 +632,18 @@ int main() {
           printf("?!\n");
         }
 
-        memset(buf, 0, sizeof(buf));
+        memset(buf, 0, buflen);
         i = 0;
-        goto next;
+        break;
       }
     }
   }
+
+  if(i) {
+    printf("incomplete input\n");
+    return -1;
+  }
+
   return 0;
 }
 #endif
